c7805db5b5d287acf7eb6e96749e931726c4e638
“krv006” <rvkamronbek@gmail.com>
2024-03-23 01:40:09 +0500

    cafe

--- cafe/cafe/settings.py
+++ cafe/cafe/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 MEDIA_URL = '/storage/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'storage')
+MEDIA_ROOT = os.path.join(BASE_DIR, )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
